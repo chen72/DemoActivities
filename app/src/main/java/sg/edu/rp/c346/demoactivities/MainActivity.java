81bc23a0102bda62e12f87089dd30b2b03c9e60e
16046469 <jie1n31997>
2018-04-16 12:42:05 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoactivities/MainActivity.java
@@ -0,0 +1,43 @@
+package sg.edu.rp.c346.demoactivities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnDone = (Button)findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                EditText etName = (EditText)findViewById(R.id.etName);
+                EditText etAge = (EditText)findViewById(R.id.etAge);
+
+                String[] info = {etName.getText().toString().trim(),
+                        etAge.getText().toString().trim()};
+
+                Intent i = new Intent(MainActivity.this,DemoActivities2.class);
+                i.putExtra("info",info);
+                startActivity(i);
+
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
